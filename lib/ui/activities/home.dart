544becf09151ca4504f518b65115b8b024dc7d2c
Thiago Oliveira <thiago.bsb@hotmail.com>
2019-11-21 01:08:39 -0300

    Componente expasion-tile-list personalizada

--- lib/ui/activities/home.dart
+++ lib/ui/activities/home.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mypresence/authentication/base_auth.dart';
+import 'package:mypresence/ui/widgets/custom_expansion_tile.dart' as cet;
+import 'package:mypresence/ui/widgets/custom_list_tile_item.dart';
 import 'package:mypresence/ui/widgets/navigation_drawer.dart';
 import 'package:mypresence/utils/colors_palette.dart';
 
@@ -24,17 +26,25 @@ class _HomeState extends State<Home> {
   /// Creates a Scaffold
   Widget _buildScaffold() {
     return Scaffold(
-        backgroundColor: ColorsPalette.backgroundColorLight,
-        appBar: _buildAppBar(),
-        drawer: NavigationDrawer(
-          user: widget.currentUser,
-          email: widget.currentUser.email,
-          photoUrl: widget.currentUser.photoUrl,
-          onSignedOut: _signedOut,
+      backgroundColor: ColorsPalette.backgroundColorLight,
+      appBar: _buildAppBar(),
+      drawer: NavigationDrawer(
+        user: widget.currentUser,
+        email: widget.currentUser.email,
+        photoUrl: widget.currentUser.photoUrl,
+        onSignedOut: _signedOut,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Container(
+            margin: new EdgeInsets.all(15.0),
+            child: Column(
+              children: _listOccurrencesWidgets(),
+            ),
+          ),
         ),
-        body: Center(
-          child: Text('Minha lista de eventos'),
-        ));
+      ),
+    );
   }
 
   ///
@@ -56,5 +66,103 @@ class _HomeState extends State<Home> {
     } catch (e) {
       debugPrint(e.toString());
     }
+  }
+
+  /// List of Occurrences (Widget)
+  List<Widget> _listOccurrencesWidgets() {
+    List<Widget> list = new List();
+
+    list.add(Container(
+          margin: new EdgeInsets.only(bottom: 10),
+          child: cet.ExpansionTile(
+        title: Text(
+          'Thursday, July 1',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        headerBackgroundColor: ColorsPalette.expansionParentHeaderColor,
+        backgroundColor: ColorsPalette.backgroundColorLight,
+        iconColor: Colors.white,
+        children: _listEventsWidgets(),
+      ),
+    ));
+
+    list.add(Container(
+          
+          margin: new EdgeInsets.only(bottom: 10),
+
+          child: cet.ExpansionTile(
+        title: Text(
+          'Friday, July 2',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        headerBackgroundColor: ColorsPalette.expansionParentHeaderColor,
+        backgroundColor: ColorsPalette.expansionParentHeaderColor,
+        iconColor: Colors.white,
+        children: _listEventsWidgets(),
+      ),
+    ));
+
+    list.add(Container(
+          margin: new EdgeInsets.only(bottom: 10),
+          child: cet.ExpansionTile(
+        title: Text(
+          'Saturday, July 3',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        headerBackgroundColor: ColorsPalette.expansionParentHeaderColor,
+        backgroundColor: ColorsPalette.expansionParentHeaderColor,
+        iconColor: Colors.white,
+        children: _listEventsWidgets(),
+      ),
+    ));
+
+list.add(Container(
+          margin: new EdgeInsets.only(bottom: 10),
+          child: cet.ExpansionTile(
+        title: Text(
+          'Monday, July 4',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        headerBackgroundColor: ColorsPalette.expansionParentHeaderColor,
+        backgroundColor: ColorsPalette.expansionParentHeaderColor,
+        iconColor: Colors.white,
+        children: _listEventsWidgets(),
+      ),
+    ));
+    return list;
+  }
+
+  /// List of Events (Widget)
+  List<Widget> _listEventsWidgets() {
+    List<Widget> list = new List();
+
+    list.add(ListTileItem(
+      colorHeader: ColorsPalette.backgroundColorLightGray,
+      eventName: 'First Event',
+      eventTime: '7:00',
+    ));
+
+    list.add(ListTileItem(
+      colorHeader: ColorsPalette.backgroundColorLight,
+      eventName: 'Second Event',
+      eventTime: '9:00',
+    ));
+
+    list.add(ListTileItem(
+      colorHeader: ColorsPalette.backgroundColorLightGray,
+      eventName: 'Third Event',
+      eventTime: '11:00',
+      divider: false,
+    ));
+
+    return list;
   }
 }

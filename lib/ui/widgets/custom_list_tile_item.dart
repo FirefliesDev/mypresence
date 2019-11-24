23eea5bcaefc4f1bd2a7f9f1ed1772b801bc5f60
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2019-11-24 00:19:37 -0300

    Changes made to CustomExpansionTile & CustomListTileItem classes

--- lib/ui/widgets/custom_list_tile_item.dart
+++ lib/ui/widgets/custom_list_tile_item.dart
@@ -1,4 +1,6 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:mypresence/utils/colors_palette.dart';
 
 /// Creates a custom list item to be used in the expansion tile (children)
 class ListTileItem extends StatelessWidget {
@@ -17,6 +19,9 @@ class ListTileItem extends StatelessWidget {
   /// The color to display the event time
   final Color colorEventTime;
 
+  /// The participants count
+  final int count;
+
   /// A divider to be added bellow the item
   final bool divider;
 
@@ -29,38 +34,80 @@ class ListTileItem extends StatelessWidget {
       this.colorEventName,
       this.colorEventTime,
       this.eventTime,
+      this.count,
       this.divider = true,
       this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
+      child: Card(
+        elevation: 5,
         color: colorHeader,
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    eventName,
-                    style: TextStyle(
-                      color: colorEventName,
+        child: Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ListTile(
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      eventName,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        color: colorEventName,
+                      ),
                     ),
+                    Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: <Widget>[
+                        Container(
+                          width: 24,
+                          height: 24,
+                          decoration: new BoxDecoration(
+                            color: ColorsPalette.accentColor,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        Text(
+                          count.toString() == 'null' ? '0' : count.toString(),
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: ColorsPalette.textColorLight),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Location',
+                        style: TextStyle(
+                          color: colorEventName,
+                        ),
+                      ),
+                      Text(
+                        eventTime,
+                        style: TextStyle(
+                          color: colorEventTime,
+                        ),
+                      )
+                    ],
                   ),
-                  Text(
-                    eventTime,
-                    style: TextStyle(
-                      color: colorEventTime,
-                    ),
-                  ),
-                ],
+                ),
               ),
-              // trailing: Icon(Icons.delete),
-              onTap: onTap,
-            ),
-            divider ? Divider(color: Colors.black, height: 0,) : SizedBox(height: 0,)
-          ],
-        ));
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

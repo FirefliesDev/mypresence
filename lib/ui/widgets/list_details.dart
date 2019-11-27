8903da3d2c3c1e8e97ff49f52b3a03c336310489
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2019-11-27 00:28:35 -0300

    Updated

--- lib/ui/widgets/list_details.dart
+++ lib/ui/widgets/list_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mypresence/ui/widgets/custom_expansion_tile.dart' as cet;
+import 'package:mypresence/utils/colors_palette.dart';
 
 class ListDetails extends StatefulWidget {
   @override
@@ -9,203 +10,208 @@ class ListDetails extends StatefulWidget {
 class _ListDetailsState extends State<ListDetails> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Row(
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: SafeArea(
+        child: Column(
           children: <Widget>[
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 7.5, horizontal: 15),
-                    width: 50.0,
-                    height: 40.0,
-                    decoration: new BoxDecoration(
-                      color: Colors.black,
-                      shape: BoxShape.rectangle,
-                    ),
-                  ),
-                  Text(
-                    'Evento 1',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-            ),
             Row(
               children: <Widget>[
-                Container(
-                  width: 30.0,
-                  height: 30.0,
-                  decoration: new BoxDecoration(
-                    color: Colors.deepOrange,
-                    shape: BoxShape.rectangle,
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: 30.0,
+                      height: 30.0,
+                      decoration: new BoxDecoration(
+                        color: Colors.deepOrange,
+                        shape: BoxShape.rectangle,
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text('Gerar QR Code'),
+                    ),
+                    Text(
+                      '12/11',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: Text(
+                'Palestra - Projeto Ágil',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: Text(
+                'Palestra sobre design, onde iremos abordar sobre todos '
+                'os tipos de prototipação',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+              ),
+            ),
+            cet.ExpansionTile(
+              title: Container(
+                child: Text(
+                  'Participantes',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 25,
                   ),
                 ),
-                Text(
-                  '12/11',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+              ),
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 7.5, horizontal: 15),
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: new BoxDecoration(
+                          color: Colors.amber,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Nome",
+                              style: TextStyle(
+                                inherit: true,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              "email",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                //REMOVER
+                Row(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 7.5, horizontal: 15),
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: new BoxDecoration(
+                          color: Colors.amber,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Nome",
+                              style: TextStyle(
+                                inherit: true,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              "email",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 7.5, horizontal: 15),
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: new BoxDecoration(
+                          color: Colors.amber,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Nome",
+                              style: TextStyle(
+                                inherit: true,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              "email",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
           ],
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-          child: Text(
-            'Palestra - Projeto Ágil',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-          child: Text(
-            'Palestra sobre design, onde iremos abordar sobre todos '
-            'os tipos de prototipação',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-          ),
-        ),
-        cet.ExpansionTile(
-          title: Container(
-            child: Text(
-              'Participantes',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w700,
-                fontSize: 25,
-              ),
-            ),
-          ),
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    margin: EdgeInsets.symmetric(vertical: 7.5, horizontal: 15),
-                    width: 50.0,
-                    height: 50.0,
-                    decoration: new BoxDecoration(
-                      color: Colors.amber,
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Nome",
-                          style: TextStyle(
-                            inherit: true,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          "email",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            //REMOVER
-            Row(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    margin: EdgeInsets.symmetric(vertical: 7.5, horizontal: 15),
-                    width: 50.0,
-                    height: 50.0,
-                    decoration: new BoxDecoration(
-                      color: Colors.amber,
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Nome",
-                          style: TextStyle(
-                            inherit: true,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          "email",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    margin: EdgeInsets.symmetric(vertical: 7.5, horizontal: 15),
-                    width: 50.0,
-                    height: 50.0,
-                    decoration: new BoxDecoration(
-                      color: Colors.amber,
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Nome",
-                          style: TextStyle(
-                            inherit: true,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          "email",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ],
+      ),
+    );
+  }
+
+  ///
+  Widget _buildAppBar() {
+    return AppBar(
+      title: Text(
+        'Evento 1',
+        style: TextStyle(color: ColorsPalette.textColorLight),
+      ),
+      iconTheme: IconThemeData(color: ColorsPalette.textColorLight),
     );
   }
 }

edc19a546e9f265fdfae0ed315c3395d232c6384
Thiago Oliveira <thiago.bsb@hotmail.com>
2019-11-28 00:59:46 -0300

    lista de detalhamento de ocorrência

--- lib/ui/widgets/list_details.dart
+++ lib/ui/widgets/list_details.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:mypresence/ui/widgets/custom_expansion_tile.dart' as cet;
 import 'package:mypresence/utils/colors_palette.dart';
 
@@ -15,33 +17,8 @@ class _ListDetailsState extends State<ListDetails> {
       body: SafeArea(
         child: Column(
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Container(
-                      width: 30.0,
-                      height: 30.0,
-                      decoration: new BoxDecoration(
-                        color: Colors.deepOrange,
-                        shape: BoxShape.rectangle,
-                      ),
-                    ),
-                    FlatButton(
-                      onPressed: () {},
-                      child: Text('Gerar QR Code'),
-                    ),
-                    Text(
-                      '12/11',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
-                    ),
-                  ],
-                ),
-              ],
-            ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              padding: const EdgeInsets.fromLTRB(25, 25, 25, 10),
               child: Text(
                 'Palestra - Projeto Ágil',
                 textAlign: TextAlign.center,
@@ -56,6 +33,56 @@ class _ListDetailsState extends State<ListDetails> {
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
               ),
+            ),
+            cet.ExpansionTile(
+              title: Container(
+                child: Text(
+                  'Lista de Ocorrências',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(
+                            "23/11",
+                            style: TextStyle(
+                              inherit: true,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "Hora",
+                            style: TextStyle(
+                              inherit: true,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "Local",
+                            style: TextStyle(
+                              inherit: true,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
             cet.ExpansionTile(
               title: Container(
@@ -200,6 +227,44 @@ class _ListDetailsState extends State<ListDetails> {
             ),
           ],
         ),
+      ),
+      floatingActionButton: SpeedDial(
+        // both default to 16
+        marginRight: 18,
+        marginBottom: 20,
+        animatedIcon: AnimatedIcons.menu_close,
+        animatedIconTheme: IconThemeData(size: 22.0),
+        // this is ignored if animatedIcon is non null
+        //child: Icon(Icons.add),
+        // If true user is forced to close dial manually
+        // by tapping main button and overlay is not rendered.
+        closeManually: false,
+        curve: Curves.bounceIn,
+        overlayColor: Colors.black,
+        overlayOpacity: 0.1,
+        onOpen: () => print('OPENING DIAL'),
+        onClose: () => print('DIAL CLOSED'),
+        tooltip: 'Speed Dial',
+        heroTag: 'speed-dial-hero-tag',
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 8.0,
+        shape: CircleBorder(),
+        children: [
+          SpeedDialChild(
+              child: Icon(Icons.person),
+              backgroundColor: Colors.green,
+              label: 'Novo Participante',
+              labelStyle: TextStyle(fontSize: 18.0),
+              onTap: () => print('add_participant')),
+          SpeedDialChild(
+            child: Icon(Icons.event),
+            backgroundColor: Colors.blue,
+            label: 'Novo Ocorrência',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('add_occurrence'),
+          ),
+        ],
       ),
     );
   }

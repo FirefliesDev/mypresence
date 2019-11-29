f145247f6c16ad0c68977b656cac310ce18f957f
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2019-11-29 00:32:31 -0300

    Constants created

---
+++ lib/utils/constants.dart
@@ -0,0 +1,15 @@
+///
+class FirebaseConstant {
+  ///
+  static const String item = "item";
+
+  ///
+  static const String event = "event";
+
+  ///
+  static const String occurrence = "occurrence";
+
+  
+  ///
+  static const String eventOccurrences = "eventOccurrences";
+}

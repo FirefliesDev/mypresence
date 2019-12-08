07c367f3873f800064e491e39a8a6c2b84fff727
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2019-12-08 04:00:28 -0300

    Adding participant's count

--- lib/model/event.dart
+++ lib/model/event.dart
@@ -2,13 +2,15 @@ class Event {
   String id;
   String title;
   String descripton;
+  String countParticipants;
 
-  Event({this.id, this.title, this.descripton});
+  Event({this.id, this.title, this.descripton, this.countParticipants});
 
   Event.fromJson(Map<dynamic, dynamic> json) {
     id = json['id'];
     title = json['title'];
     descripton = json['descripton'];
+    countParticipants = json['count_participants'];
   }
 
   Map<dynamic, dynamic> toJson() {
@@ -16,6 +18,7 @@ class Event {
     data['id'] = this.id;
     data['title'] = this.title;
     data['descripton'] = this.descripton;
+    data['count_participants'] = this.countParticipants;
     return data;
   }
 }
